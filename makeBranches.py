0d44cd0fdd44b889dbf7ca80022dea9715e435c5
Lyla McBeath Fujiwara <lmf@google.com>
2018-08-10 16:34:54 -0700

    cleanup

--- makeBranches.py
+++ makeBranches.py
@@ -16,10 +16,6 @@ IS_BRANCH_STRING = "Step"
 
 MAX_LENGTH = 100
 
-#STUDENT="student"
-#DEVELOP="develop-"
-#DEVELOP_DEFAULT = "all develop branches"
-
 def makeBranches(repo_directory, branch_code_steps):
     print repo_directory
     print branch_code_steps
@@ -37,9 +33,7 @@ def makeBranches(repo_directory, branch_code_steps):
             new_branch = repo.create_head(branch_name)
             new_branch.set_commit(commitHash)
 
-            # repo.git.checkout(commit)
             # print "Would create: ", branch_name, " for commit ", commitHash 
-            # repo.git.clean("-fdx")
             
 
 def get_branch_name_from_commit_message(message):
@@ -51,108 +45,6 @@ def get_branch_name_from_commit_message(message):
             MAX_LENGTH else safe_message)
 
 
-# def flatten(repo_dir, student, develop_branches, remove_branches):
-
-#     repo = Repo(repo_dir)
-
-
-#     if develop_branches == DEVELOP_DEFAULT:
-#         develop_branches = [branch for branch in repo.branches if DEVELOP in branch.name]
-
-#     if remove_branches:
-#         remove_local_branches(repo, student, develop_branches)
-
-#     flat = len(develop_branches) == 1
-
-#     # print develop_branches
-
-#     try:
-#         temp_dir = tempfile.mkdtemp()
-#         try:
-#             current_branch = repo.active_branch
-#             print "Stashing"
-#             repo.git.stash()
-
-#             for develop in develop_branches:
-#                 to_temp_dir(repo, repo_dir, develop, temp_dir, flat)
-#             # insert_diff_links(temp_dir)
-#             snapshots_to_student_branch(repo, student, temp_dir, repo_dir)
-#         finally:
-#             repo.git.checkout(current_branch)
-#             print "Popping"
-#             if repo.git.stash("list"):
-#                 repo.git.stash("pop")
-#     finally:
-#         if os.path.exists(temp_dir):
-#             shutil.rmtree(temp_dir)
-
-#     print "Done! Review and commit the", student, "branch at your leisure."
-#     print "Then run $ git push --all --prune"
-
-
-# def remove_local_branches(repo, student, develop_branches):
-#     for branch in repo.branches:
-#         if branch.name != student and branch not in develop_branches:
-#             print "Removing local branch:", branch.name
-#             repo.git.branch(branch.name, "-D")
-
-
-# def to_temp_dir(repo, repo_dir, develop, temp_dir, flat):
-#     for rev in repo.git.rev_list(develop).split("\n"):
-#         commit = repo.commit(rev)
-#         branch_name = get_branch_name_from_commit_message(commit.message)
-#         if "Exercise" in branch_name or "Solution" in branch_name:
-#             if branch_name in repo.branches:
-#                 repo.git.branch(branch_name, "-D")
-#             new_branch = repo.create_head(branch_name)
-#             new_branch.set_commit(rev)
-
-#             repo.git.checkout(commit)
-#             print "Saving snapshot of:", branch_name
-#             repo.git.clean("-fdx")
-#             if flat:
-#                 target_dir = os.path.join(temp_dir, branch_name)
-#             else:
-#                 _, folder_name = develop.name.split("-")
-#                 target_dir = os.path.join(temp_dir, folder_name, branch_name)
-
-#             shutil.copytree(repo_dir, target_dir,
-#                             ignore=shutil.ignore_patterns(*IGNORE_PATTERNS))
-#             with open(os.path.join(target_dir, "README.md"), "a") as readme:
-#                 print branch_name
-#                 number, _, name = branch_name.split("-")
-#                 readme.write(DIFF_FORMAT.format(number=number, name=name))
-
-
-
-
-
-# DIFF_FORMAT = """
-
-# You can download a zip of this exercise [here](https://github.com/udacity/ud843-QuakeReport/archive/{number}-Exercise-{name}.zip), \
-# and a zip of the solution [here](https://github.com/udacity/ud843-QuakeReport/archive/{number}-Solution-{name}.zip). \
-# Also, you can find a visual summary of the solution [here](https://github.com/udacity/ud843-QuakeReport/compare/{number}-Exercise-{name}...{number}-Solution-{name}).
-
-# """
-
-
-# def insert_diff_links(temp_dir):
-#     for item in os.listdir(temp_dir):
-#         number, _, name = item.split("-")
-#         with open(os.path.join(temp_dir, item, "README.md"), "a") as readme:
-#             readme.write(DIFF_FORMAT.format(number=number, name=name))
-
-
-# def snapshots_to_student_branch(repo, student, temp_dir, repo_dir):
-#     repo.git.checkout(student)
-#     for item in os.listdir(temp_dir):
-#         source_dir = os.path.join(temp_dir, item)
-#         target_dir = os.path.join(repo_dir, item)
-
-#         if os.path.exists(target_dir):
-#             shutil.rmtree(target_dir)
-#         print "Copying: ", item
-#         shutil.copytree(source_dir, target_dir)
 
 
 DESCRIPTION = "This script "
@@ -189,13 +81,6 @@ def main():
         parsed.branch
         )
 
-
-    # flatten(
-    #     parsed.directory,
-    #     parsed.student,
-    #     parsed.develop_branches,
-    #     parsed.remove
-    # )
 
 if __name__ == "__main__":
     sys.exit(main())
